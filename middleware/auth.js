a0fcf12daf4105671e410cc147c9e917bd6ae515
BilalAbbas464 <bilalabbas11221133@gmail.com>
2025-02-13 22:38:30 +0500

    Authorization Headers

--- middleware/auth.js
+++ middleware/auth.js
@@ -1,18 +1,21 @@
 const {get_User} = require('../service/auth')
 
 async function restirectToLoggedInUser(req,res,next) {
-    const Id = req.cookies?.id
-    if(!Id) return res.redirect('/login')
-    const find = get_User(Id)
-    if(!find) return res.redirect('/login')
-    req.user = find
+    const Id = req.headers['Authorization'];
+    console.log(Id)
+    if(!Id) return res.redirect('/login');
+    const token = Id.split('Bearer ')[1];
+    const find = get_User(token);
+    if(!find) return res.redirect('/login');
+    req.user = find;
     next()
 }
 
 async function check_Auth(req,res,next) {
-    const Id = req.cookies?.id
-    const find = get_User(Id)
-    req.user = find
+    const Id = req.headers['authorization'];
+    const token = Id.split('Bearer ')[1];
+    const find = get_User(token);
+    req.user = find;
     next()
 }
 
