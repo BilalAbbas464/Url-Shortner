62871285e48ae6e6d965bc21628a8c9ef1f91266
BilalAbbas464 <bilalabbas11221133@gmail.com>
2025-02-15 18:59:04 +0500

    Safety Check

--- middleware/auth.js
+++ middleware/auth.js
@@ -11,7 +11,7 @@ function checkForAuthentication(req,res,next){
     return next()
 }
 
-function restrictTo(roles){
+function restrictTo(roles=[]){
     return function(req,res,next){
         if(!req.user) return res.redirect('/login')
         if(roles.includes(req.user.role)) return res.end("UnAuthorized")
